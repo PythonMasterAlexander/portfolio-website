93e2cf53d3b086fbdf17581a761ddc0b57b387cc
alexander <alexanderhardeland@gmail.com>
2023-03-08 08:11:52 +0100

    Added a click on button transform effect. But not happy with it so will create a issue and ask for a teach to help me out

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,4 @@
 import addPageMenuFunctionality from './components/addPageMenuFunctionality.js';
 import { submitMessageButton } from './constants/variables.js';
-const messageButtonContainer = document.querySelector(".message-button");
-console.log(messageButtonContainer);
-
-submitMessageButton.addEventListener("click", function() {
-  event.preventDefault();
-  console.log(event.target);
-  messageButtonContainer.classList.toggle("test");
-}); 
 
 addPageMenuFunctionality();
