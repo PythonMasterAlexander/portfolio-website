4e76bf2dd7c804da6d3a4873278a3411e497d8f0
alexander <alexanderhardeland@gmail.com>
2023-03-15 18:17:59 +0100

    Nothing changes, only writing some test code in typescript

--- src/scripts/constants/variables.ts
+++ src/scripts/constants/variables.ts
@@ -1,3 +1,13 @@
 export const mainMenuButton = document.querySelector(".menu-button") as HTMLButtonElement;
 export const pageMenuContainer = document.querySelector(".page-menu-container") as HTMLDivElement;
 export const headerSocialMediaContainer = document.querySelector(".header-social-media-container") as HTMLDivElement;
+
+
+/*
+ syntax for writing a list and looping over
+const test: (string | number | boolean)[] = ["hello", 23, true];
+for (let i: number = 0; i < test.length; i ++) {
+  console.log(test[i]);
+}
+ syntax for writing a object and looping over
+*/
