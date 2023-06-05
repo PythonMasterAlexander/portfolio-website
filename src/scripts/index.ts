da1327b3cc37a1e7f76a1693806894963220238e
alexander <alexanderhardeland@gmail.com>
2023-06-05 14:07:45 +0200

    Trying to get JSdocs to work. But because file paths are wrong I cannot get it to work now

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,9 @@
 import openMainMenu from './components/openMainMenu.js';
-import { mainMenuButton, pageMenuContainer } from './constants/variables.js';
+import { mainMenuButton } from './constants/variables.js';
 
+/**
+  * Adds an event listener to the main button on the page
+  * The event listener listen for when the user click the menu
+*/
 mainMenuButton.addEventListener("click", openMainMenu);
 
